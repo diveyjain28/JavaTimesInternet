504aae02c5de7c39bad33bf109054f77dbfeae50
diveyjain28 <diveyjain28@gmail.com>
2021-08-09 14:22:58 +0530

    2nd initial commit

---
+++ src/main/java/com/til/socialapp/TilSocial/TilSocialApplication.java
@@ -0,0 +1,13 @@
+package com.til.socialapp.TilSocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TilSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/til/socialapp/TilSocial/TilSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.til.socialapp.TilSocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TilSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
